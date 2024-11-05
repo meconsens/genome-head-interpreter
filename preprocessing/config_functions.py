bd9f8a144fe46649d8a0ff909031d411937536ea
meconsens <micaela.consens@mail.utoronto.ca>
2024-11-05 08:13:17 -0500

    move config functions to helper

---
+++ preprocessing/config_functions.py
@@ -0,0 +1,52 @@
+
+import numpy as np
+
+#set up for different models configurations
+def configure_DNABERT(df):
+    #copy to modify
+    df_copy = df.copy()
+    #print all the column names
+    print(df_copy.columns)
+    for col in df_copy.columns:
+        if col not in ['sequence', 'kmers']:
+            df_copy[col] = df_copy[col].apply(lambda x: np.array(x.split(','), dtype=np.float64))
+    return {
+        'attention_score_columns': [col for col in df_copy.columns if 'layer' in col and 'head' in col],
+        'bio_feature_columns': [col for col in df_copy.columns if not (('layer' in col and 'head' in col) or ('label' in col) or ('sequence' in col) or ('kmers' in col))],
+        'seq_length': df_copy['kmers'].apply(lambda x: len(x.split(','))).tolist(),  # sequence lengths based on 'kmers',
+        'seq_column': 'sequence',
+        'transformed_df': df_copy, # transformed df
+    }
+
+
+def configure_enformer(df):
+    # copy to modify
+    df_copy = df.copy()
+    
+    for col in df_copy.columns:
+        if col != 'gene':
+            df_copy[col] = df_copy[col].apply(lambda x: np.array(x.split(','), dtype=np.float64))
+    
+    return {
+        'attention_score_columns': [col for col in df_copy.columns if 'layer' in col and 'head' in col],
+        'bio_feature_columns': [col for col in df_copy.columns if not (('layer' in col and 'head' in col) or ('gene' in col))],
+        'seq_length': 1536,
+        'seq_column': 'gene',
+        'transformed_df': df_copy  # transformed df
+    }
+
+def configure_scgpt(df):
+    df_copy = df.copy()
+    for col in df_copy.columns:
+        if col not in ['gene_sequence', 'label']:
+            df_copy[col] = df_copy[col].apply(lambda x: np.array(x.split(','), dtype=np.float64))
+    return {
+        'attention_score_columns': [col for col in df_copy.columns if 'layer' in col and 'head' in col],
+        'bio_feature_columns': [col for col in df_copy.columns if not (('layer' in col and 'head' in col) or ('gene_sequence' in col) or  ('label' in col))],
+        'seq_length': 500,
+        'seq_column': 'gene_sequence',
+        'transformed_df': df_copy # transformed df
+    }
+
+#export all functions
+__all__ = ['configure_DNABERT', 'configure_enformer', 'configure_scgpt']
