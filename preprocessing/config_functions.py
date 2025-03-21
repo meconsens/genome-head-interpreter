ee6123ba24771f5e6459a3080fcceb6d40f68dd3
meconsens <micaela.consens@mail.utoronto.ca>
2025-03-21 09:23:30 -0400

    update for nt, and new model subtypes

--- preprocessing/config_functions.py
+++ preprocessing/config_functions.py
@@ -9,7 +9,12 @@ def configure_DNABERT(df):
     print(df_copy.columns)
     for col in df_copy.columns:
         if col not in ['sequence', 'kmers']:
-            df_copy[col] = df_copy[col].apply(lambda x: np.array(x.split(','), dtype=np.float64))
+            # Check if the column contains string data before attempting to split
+            if df_copy[col].dtype == 'object':
+                df_copy[col] = df_copy[col].apply(lambda x: np.array(x.split(','), dtype=np.float64) if isinstance(x, str) else np.array([x], dtype=np.float64))
+            else:
+                # For non-string data, convert directly to numpy arrays
+                df_copy[col] = df_copy[col].apply(lambda x: np.array([x], dtype=np.float64))
     return {
         'attention_score_columns': [col for col in df_copy.columns if 'layer' in col and 'head' in col],
         'bio_feature_columns': [col for col in df_copy.columns if not (('layer' in col and 'head' in col) or ('label' in col) or ('sequence' in col) or ('kmers' in col))],
@@ -18,6 +23,26 @@ def configure_DNABERT(df):
         'transformed_df': df_copy, # transformed df
     }
 
+def configure_nucleotide_transformer(df):
+    #copy to modify
+    df_copy = df.copy()
+    #print all the column names
+    print(df_copy.columns)
+    for col in df_copy.columns:
+        if col not in ['sequence', 'kmers']:
+            # Check if the column contains string data before attempting to split
+            if df_copy[col].dtype == 'object':
+                df_copy[col] = df_copy[col].apply(lambda x: np.array(x.split(','), dtype=np.float64) if isinstance(x, str) else np.array([x], dtype=np.float64))
+            else:
+                # For non-string data, convert directly to numpy arrays
+                df_copy[col] = df_copy[col].apply(lambda x: np.array([x], dtype=np.float64))
+    return {
+        'attention_score_columns': [col for col in df_copy.columns if 'layer' in col and 'head' in col],
+        'bio_feature_columns': [col for col in df_copy.columns if not (('layer' in col and 'head' in col) or ('label' in col) or ('sequence' in col) or ('kmers' in col))],
+        'seq_length': df_copy['kmers'].apply(lambda x: len(x.split(','))).tolist(),  # sequence lengths based on 'kmers',
+        'seq_column': 'sequence',
+        'transformed_df': df_copy, # transformed df
+    }
 
 def configure_scgpt(df):
     df_copy = df.copy()
